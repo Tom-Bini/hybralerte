3fbf48b52f8a42afd8aa206a4b9af2e1ec6b36d0
Tom-Bini <bini.tom04@gmail.com>
2025-07-18 15:57:22 +0200

    Added Hype price in top right corner

---
+++ hypePrice.js
@@ -0,0 +1,36 @@
+async function fetchHypePrice() {
+  const endpoint = "https://api.goldsky.com/api/public/project_cmbj707z4cd9901sib1f6cu0c/subgraphs/hybra-v3/v3/gn";
+
+  const payload = {
+    operationName: "getEthPrice",
+    variables: {},
+    query: `query getEthPrice {
+      bundle(id: "1") {
+        ethPriceUSD
+      }
+    }`
+  };
+
+  try {
+    const res = await fetch(endpoint, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Origin": "https://www.hybra.finance",
+        "Referer": "https://www.hybra.finance/"
+      },
+      body: JSON.stringify(payload)
+    });
+
+    const data = await res.json();
+    const price = parseFloat(data?.data?.bundle?.ethPriceUSD ?? 0);
+    document.getElementById("hypePriceBadge").textContent =
+      `💰 Prix du $HYPE : $${price.toFixed(2)}`;
+  } catch (err) {
+    console.error("Erreur fetch $HYPE :", err);
+    document.getElementById("hypePriceBadge").textContent =
+      `💰 Prix du $HYPE : erreur`;
+  }
+}
+fetchHypePrice();
+setInterval(fetchHypePrice, 5 * 1000);
